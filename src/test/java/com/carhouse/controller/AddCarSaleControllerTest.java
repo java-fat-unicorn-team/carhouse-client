ccd4a65b1a5effb5a8bd43c96da69f82d5346e0e
maksimk99 <maksimilian.koturanov@mail.ru>
2019-08-27 16:55:13 +0300

    add tests for controllers

---
+++ src/test/java/com/carhouse/controller/AddCarSaleControllerTest.java
@@ -0,0 +1,68 @@
+package com.carhouse.controller;
+
+import com.carhouse.model.CarMake;
+import com.carhouse.model.FuelType;
+import com.carhouse.provider.CarMakeProvider;
+import com.carhouse.provider.FuelTypeProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class AddCarSaleControllerTest {
+
+    @Mock
+    private CarMakeProvider carMakeProvider;
+    @Mock
+    private FuelTypeProvider fuelTypeProvider;
+    @InjectMocks
+    private AddCarSaleController addCarSaleController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(addCarSaleController).build();
+    }
+
+    @Test
+    void addCarSale() throws Exception {
+        List<CarMake> listCarMake = new ArrayList<>() {{
+            add(new CarMake(0, "Bentley"));
+            add(new CarMake(1, "BMW"));
+        }};
+        List<FuelType> listFuelType = new ArrayList<>() {{
+            add(new FuelType(0, "Petrol"));
+            add(new FuelType(1, "Diesel"));
+        }};
+        when(carMakeProvider.getCarMakes()).thenReturn(listCarMake);
+        when(fuelTypeProvider.getFuelTypes()).thenReturn(listFuelType);
+        mockMvc.perform(get("/carSale/add"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("addCarSale"))
+                .andExpect(model().attribute("listCarMakes", listCarMake))
+                .andExpect(model().attribute("listFuelTypes", listFuelType));
+    }
+
+    @Test
+    void addCarSaleSubmit() throws Exception {
+        mockMvc.perform(post("/carSale/add"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/homePage"));
+    }
+}
---
+++ src/test/java/com/carhouse/controller/AjaxRestControllerTest.java
@@ -0,0 +1,53 @@
+package com.carhouse.controller;
+
+import com.carhouse.model.CarMake;
+import com.carhouse.model.CarModel;
+import com.carhouse.provider.CarModelProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class AjaxRestControllerTest {
+
+    @Mock
+    private CarModelProvider carModelProvider;
+
+    @InjectMocks
+    private AjaxRestController ajaxRestController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(ajaxRestController).build();
+    }
+
+    @Test
+    void getCarModels() throws Exception {
+        List<CarModel> listCarModels = new ArrayList<>() {{
+            add(new CarModel(0, new CarMake(), "M2"));
+            add(new CarModel(1, new CarMake(), "M4"));
+        }};
+        when(carModelProvider.getCarModels(2)).thenReturn(listCarModels);
+        mockMvc.perform(get("/carModels/{carMakeId}", 2))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(listCarModels)));
+    }
+}
