98c05155c08009a09a6905d297893eb12f5e7d52
maksimk99 <maksimilian.koturanov@mail.ru>
2019-09-02 17:34:33 +0300

    change validation of car make and car model id

--- src/main/java/com/carhouse/provider/impl/CarModelProviderImpl.java
+++ src/main/java/com/carhouse/provider/impl/CarModelProviderImpl.java
@@ -2,6 +2,7 @@ package com.carhouse.provider.impl;
 
 import com.carhouse.model.CarModel;
 import com.carhouse.provider.CarModelProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -20,12 +21,8 @@ public class CarModelProviderImpl implements CarModelProvider {
      * @return the car model
      */
     public CarModel getCarModel(final String carModelId) {
-        if (carModelId != null && !carModelId.isEmpty()) {
-            try {
-                return new CarModel().setCarModelId(Integer.parseInt(carModelId)).setCarModel("M2");
-            } catch (NumberFormatException ex) {
-                return null;
-            }
+        if (StringUtils.isNumeric(carModelId)) {
+            return new CarModel().setCarModelId(Integer.parseInt(carModelId)).setCarModel("M2");
         } else {
             return null;
         }
@@ -38,20 +35,15 @@ public class CarModelProviderImpl implements CarModelProvider {
      * @return the list car model
      */
     public List<CarModel> getCarModels(final String carMakeId) {
-        if (carMakeId != null && !carMakeId.isEmpty()) {
-            try {
+        if (StringUtils.isNumeric(carMakeId)) {
             return new ArrayList<>() {{
                 add(new CarModel().setCarModelId(0).setCarModel("M2"));
                 add(new CarModel().setCarModelId(1).setCarModel("M4"));
                 add(new CarModel().setCarModelId(2).setCarModel("M5"));
                 add(new CarModel().setCarModelId(3).setCarModel("M6"));
             }};
-            } catch (NumberFormatException ex) {
-                return null;
-            }
         } else {
             return new ArrayList<>();
         }
-
     }
 }
