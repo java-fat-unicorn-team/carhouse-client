7f6a10fb9d2a2b68fc189d8773490fd2b1e5b17c
maksimk99 <maksimilian.koturanov@mail.ru>
2019-09-30 20:26:10 +0300

    change endpoints according to the review #12

--- src/test/java/com/carhouse/controller/HomeControllerTest.java
+++ src/test/java/com/carhouse/controller/HomeControllerTest.java
@@ -2,7 +2,6 @@ package com.carhouse.controller;
 
 import com.carhouse.handler.RestTemplateResponseErrorHandler;
 import com.carhouse.model.CarMake;
-import com.carhouse.model.dto.ExceptionJSONResponse;
 import com.carhouse.provider.CarMakeProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,10 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 
 import java.util.ArrayList;
